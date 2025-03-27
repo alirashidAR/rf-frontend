070c764d5948d8efd9f628c11d792e8d022a7440
Tanisha <pahwatanisha@gmail.com>
2025-03-28 02:18:05 +0530

    minor fix

--- src/components/ecommerce/RecentOrders.tsx
+++ src/components/ecommerce/RecentOrders.tsx
@@ -121,7 +121,7 @@ export default function RecentOrders() {
             <TableRow>
               <TableCell
                 isHeader
-                className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400 w-[300px]"
               >
                 Projects
               </TableCell>
